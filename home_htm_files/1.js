07eee83e0c8e98eacf3f3304433502b65cb0331d
Anirban Samanta <asamanta@ucdavis.edu>
2018-09-21 23:44:48 -0700

    ecegsa_testsite_v0.1

---
+++ home_htm_files/1.js
@@ -0,0 +1,42 @@
+window.local_imageGallerySimple_v11_1_data = [
+{
+ border: "4",
+ cols: "4",
+ effect: "1",
+ popupsize: "750",
+ rotation: "-360",
+ shadowblur: "4",
+ shadowdim: "4",
+ trans: "0.3"
+},
+{
+ image: "home_htm_files/1322.jpg"
+},
+{
+ image: "home_htm_files/1323.jpg"
+},
+{
+ image: "home_htm_files/1324.jpg"
+},
+{
+ image: "home_htm_files/1325.jpg"
+},
+{
+ image: "home_htm_files/1326.jpg"
+},
+{
+ image: "home_htm_files/1327.jpg"
+},
+{
+ image: "home_htm_files/1328.jpg"
+},
+{
+ image: "home_htm_files/1329.jpg"
+},
+{
+ image: "home_htm_files/1330.jpg"
+},
+{
+ image: "home_htm_files/1331.jpg"
+}
+];
